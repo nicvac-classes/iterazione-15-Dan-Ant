d6d722ea24bb7655e644947d052343f3d30cf1ee
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2024-10-23 13:45:58 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,61 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
+string nome;
+int i, N;
+cout << "Quanti atleti ci sono in gara?" << endl;
+cin >> N;
+for (i=1; i<N; i=1+i)
+{
+    cout << "Inserisci il nome dell'atleta" << endl;
     cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    cout << "Ciao" << nome;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+cout << "esercizio due" << endl;
+
+
+int N, M, i;
+cout << "Elenco i numri da N a M" << endl;
+cout<< "Inserisci N" << endl;
+cin >> N;
+cout << "Inserisci M" << endl;
+cin >> M;
+for (i=N; i<=M; i=i+2)
+{
+    cout << i;
+}
+
+
+cout << "esercizio tre" << endl;
+
+
+int i, N, contpari, contdispari, restodue, x;
+do 
+{
+    cout <<"Quanti numeri generare?"<< endl;
+    cin >> N;
+}
+while (N<0);
+contpari = 0;
+contdispari = 0;
+for (i=0; i<N; i+1)
+{
+    x = random(1001);
+    restodue = x%2;
+    if (restodue == 0)
+    {
+        contpari = contpari +1;
+    }
+    else 
+    {
+        contdispari = contdispari+1;
+    }
+    cout << i+1 << "° numero generato: " << x << endl;
+}
+cout << "Sono stati generati " << contpari << " numeri pari e " << contdispari << " numeri dispari" << endl;
+
+}
+
+
